6b4aa4297f393904ab37a7793406d448cc6bccb4
MoAlJo <morghanjones@icloud.com>
2016-06-28 00:43:14 +0900

    Project's Initial Commit

---
+++ www/scripts/myscript.js
@@ -0,0 +1,21 @@
+function Incorrect(){
+    //grab the element we want to manipulate
+    var wrongAnswer = document.getElementById("wrong1");
+    wrongAnswer.style.color="red";
+
+}
+function Wrong(){
+    var wrongAnswer = document.getElementById("wrong2");
+    wrongAnswer.style.color="red";
+
+}
+function Notright(){
+    var wrongAnswer = document.getElementById("wrong3");
+    wrongAnswer.style.color="red";
+
+}
+
+function Correct(){
+    var correctAnswer = document.getElementById("correct1");
+    correctAnswer.style.color="green";
+}
